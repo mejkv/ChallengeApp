28284386a226161f8403fbfec0a0d452a496dc90
mejkv <m.miszczyszyn77@gmail.com>
2023-03-09 18:35:35 +0100

    Dzień 5

---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,12 @@
+﻿var number = 166354780;
+var NumberToString = number.ToString();
+char[] numbers = NumberToString.ToArray();
+int[] numbercount = new int[10];
+foreach (char digit in numbers)
+{
+    int digits = int.Parse(digit.ToString());
+    numbercount[digits]++;
+}
+Console.WriteLine("W liczbie  {0}", number);
+for (int i = 0; i < numbercount.Length; i++)
+    Console.WriteLine("Cyfra {0} występuje {1} razy", i, numbercount[i]);
