5706d0a9eefeb89802ec3a2d48de76ed1504c092
mejkv <m.miszczyszyn77@gmail.com>
2023-03-09 15:32:33 +0100

    Dzień 4

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,9 @@
+﻿var name = "Michał";
+var gender = "mężczyzna";
+var age = 23;
+if (gender == "kobieta" && age < 30)
+    Console.WriteLine("Kobieta poniżej 30");
+else if (age < 18 && gender == "mężczyzna")
+    Console.WriteLine("Niepełnoletni mężczyzna");
+else
+    Console.WriteLine(name + ", lat " + age);
