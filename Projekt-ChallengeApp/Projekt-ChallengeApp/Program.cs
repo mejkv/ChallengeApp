2b0579eefe48ae427b57abcefa3ef16a704edc1c
mejkv <m.miszczyszyn77@gmail.com>
2023-03-13 16:25:49 +0100

    Zadanie 6 - challengeapp

---
+++ Projekt-ChallengeApp/Projekt-ChallengeApp/Program.cs
@@ -0,0 +1,29 @@
+﻿//Program 
+using Projekt_ChallengeApp;
+
+Employee user1 = new Employee("michał","1234", 23);
+Employee user2 = new Employee("mikołaj", "1234", 18);
+Employee user3 = new Employee("marek", "1234", 22);
+
+List<Employee> employees = new List<Employee>() { user1, user2, user3 };
+
+for (int i = 0; i < 5; i++)
+{
+    user1.AddScore(i+5);
+	user2.AddScore(i+2);
+	user3.AddScore(i+4);
+}
+
+int bestresultd = 0;
+Employee best = null;	
+
+foreach (Employee employee in employees)
+{
+	if (employee.Result > bestresultd)
+	{
+		best = employee;
+		bestresultd = employee.Result;
+	}
+}
+
+Console.WriteLine("Najlepszy wynik ma: {0} i wynosi: {1}", best.Login, bestresultd);
---
+++ Projekt-ChallengeApp/Projekt-ChallengeApp/user.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Sources;
+
+namespace Projekt_ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+        public Employee(string login, string password, int age)
+        {
+            this.Login = login;
+            this.Password = password;
+            this.Age = age;
+        }
+
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public int Age { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+    }
+}
