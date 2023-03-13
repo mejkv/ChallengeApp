e1f64694de1641469c8034bfbc5808789826887c
mejkv <m.miszczyszyn77@gmail.com>
2023-03-13 17:03:53 +0100

    Zadanie 6 + test

---
+++ Projekt-ChallengeApp/Projekt-ChallengeApp.Tests/EmployeeTest.cs
@@ -0,0 +1,26 @@
+
+namespace Projekt_ChallengeApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenUserCollectScores_ShouldCorrectResult()
+        {
+            // arrange - przygotowanie 
+            var user= new Employee("Adam", "asdqwe", 23);
+            user.AddScore(1);
+            user.AddScore(2);
+
+            // acc - uruchomienie
+            var result = user.Result;
+            
+            // asset - sprawdzenie warunków
+            Assert.AreEqual(3, result);
+        }
+    }
+}
