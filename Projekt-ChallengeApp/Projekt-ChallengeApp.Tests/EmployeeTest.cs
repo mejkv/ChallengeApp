cd669a0e18f3d5ce0f5ae94f2db76a324b493d26
mejkv <m.miszczyszyn77@gmail.com>
2023-03-14 17:20:18 +0100

    tests

--- Projekt-ChallengeApp/Projekt-ChallengeApp.Tests/EmployeeTest.cs
+++ Projekt-ChallengeApp/Projekt-ChallengeApp.Tests/EmployeeTest.cs
@@ -1,4 +1,6 @@
 
+using System.Threading.Tasks.Sources;
+
 namespace Projekt_ChallengeApp.Tests
 {
     public class EmployeeTests
@@ -19,44 +21,89 @@ namespace Projekt_ChallengeApp.Tests
         }
 
         [Test]
-        public void WhenGetStatisticsCalled_ShouldReturnCorrectMax()
+        public void GetStatisticsMin_ShouldReturnMinValue()
         {
             var employee = new Employee("Jan", "Kowalski");
             employee.AddScore(10);
             employee.AddScore(9);
             employee.AddScore(8);
+
             var statistics = employee.GetStatistics();
 
-            // assert
-            Assert.AreEqual(10, statistics.Max);
-        }
-
-        [Test]
-        public void WhenGetStatisticsCalled_ShouldReturnCorrectMin()
-
-        {
-            var employee = new Employee("Jan", "Kowalski");
-            employee.AddScore(10);
-            employee.AddScore(9);
-            employee.AddScore(8);
-            var statistics = employee.GetStatistics();
-
-            // assert
-            
             Assert.AreEqual(8, statistics.Min);
         }
 
         [Test]
-        public void WhenGetStatisticsCalled_ShouldReturnCorrectAverage()
+        public void GetStatisticsMax_ShouldReturnMaxValue()
         {
             var employee = new Employee("Jan", "Kowalski");
             employee.AddScore(10);
             employee.AddScore(9);
             employee.AddScore(8);
-            var statistics = employee.GetStatistics();
 
-            // assert
-            Assert.AreEqual(Math.Round(3.33, 2), Math.Round(statistics.Average, 2));
+            var result = employee.GetStatistics();
+
+            Assert.AreEqual(10, result.Max);
+        }
+
+        [Test]
+        public void GetStatisticsAverage_ShouldReturnAverageValue()
+        {
+            var employee = new Employee("Jan", "Kowalski");
+            employee.AddScore(10);
+            employee.AddScore(9);
+            employee.AddScore(8);
+
+            var result = employee.GetStatistics();
+
+            Assert.AreEqual(9, result.Average);
+        }
+
+        [Test]
+        public void GetStatisticsWithStringOrChar_ShouldReturnScore()
+        {
+            var employee = new Employee("Jan", "Kowalski");
+            employee.AddScore('a');
+            employee.AddScore('B');
+            employee.AddScore('c');
+
+            var result = employee.GetStatistics();
+
+            Assert.AreEqual(80, result.Average);
+        }
+
+        [Test]
+        public void GetStatisticsSum_ShouldReturnCorrectSum()
+        {
+            var employee = new Employee("Jan", "Kowalski");
+            employee.AddScore("a");
+            employee.AddScore("B");
+            employee.AddScore("c");
+            employee.AddScore("27");
+            employee.AddScore("6.2f");
+            employee.AddScore("5.3f");
+            employee.AddScore("13.77");
+
+            var result = employee.GetStatistics();
+
+            Assert.AreEqual(40.77f, result.Sum);
+        }
+
+        [Test]
+        public void GetStatisticsAveverageLetter_ShouldReturnLetterForCorrectAverage()
+        {
+            var employee = new Employee("Jan", "Kowalski");
+            employee.AddScore('a');
+            employee.AddScore('B');
+            employee.AddScore('c');
+            employee.AddScore("27");
+            employee.AddScore("6.2f");
+            employee.AddScore("5.3f");
+            employee.AddScore("13.77");
+
+            var result = employee.GetStatistics();
+
+            Assert.AreEqual('C', result.AverageLetter);
         }
     }
 }
