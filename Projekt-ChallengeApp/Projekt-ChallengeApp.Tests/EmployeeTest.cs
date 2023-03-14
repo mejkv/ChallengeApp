5fccc1f3183b7d139401bad3109620e3922de854
mejkv <m.miszczyszyn77@gmail.com>
2023-03-14 08:13:11 +0100

    added dec to float, double to float, char to float and string to float

--- Projekt-ChallengeApp/Projekt-ChallengeApp.Tests/EmployeeTest.cs
+++ Projekt-ChallengeApp/Projekt-ChallengeApp.Tests/EmployeeTest.cs
@@ -13,9 +13,50 @@ namespace Projekt_ChallengeApp.Tests
             employee.AddScore(8);
             var statistics = employee.GetStatistics();
 
-            // assert - sprawdzenie warunków
+            // assert
             Assert.IsNotNull(statistics);
             Assert.That(statistics.Average, Is.Positive);
+        }
+
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldReturnCorrectMax()
+        {
+            var employee = new Employee("Jan", "Kowalski");
+            employee.AddScore(10);
+            employee.AddScore(9);
+            employee.AddScore(8);
+            var statistics = employee.GetStatistics();
+
+            // assert
+            Assert.AreEqual(10, statistics.Max);
+        }
+
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldReturnCorrectMin()
+
+        {
+            var employee = new Employee("Jan", "Kowalski");
+            employee.AddScore(10);
+            employee.AddScore(9);
+            employee.AddScore(8);
+            var statistics = employee.GetStatistics();
+
+            // assert
+            
+            Assert.AreEqual(8, statistics.Min);
+        }
+
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldReturnCorrectAverage()
+        {
+            var employee = new Employee("Jan", "Kowalski");
+            employee.AddScore(10);
+            employee.AddScore(9);
+            employee.AddScore(8);
+            var statistics = employee.GetStatistics();
+
+            // assert
+            Assert.AreEqual(Math.Round(3.33, 2), Math.Round(statistics.Average, 2));
         }
     }
 }
